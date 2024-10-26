433f400b125d73d82a08ad0428bb7bf10163add0
PouryaCh <pouryachary.8228@gmail.com>
2024-10-26 17:00:31 +0330

    fix/feat:pourya-fix and change binance app

--- binance_project/binance/views.py
+++ binance_project/binance/views.py
@@ -52,55 +52,51 @@ from rest_framework import status
 def fetch_data(url):
     try:
         response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {"error": f"Request failed with status code {response.status_code}"}
-    except Exception as e:
-        print(f"Error fetching data from Binance: {e}")
-    return None
+        response.raise_for_status() 
+        return response.json()  
+    except requests.exceptions.RequestException as e:
+        return {'error': str(e)} 
 
 
 class BinancePairs(APIView):
     
     def get(self, request):
         url = 'https://api.binance.com/api/v3/ticker/price'
-        num_requests = 20  # تعداد درخواست‌ها
-        num_threads = 20  # تعداد ترد‌های همزمان
-        delay_between_batches = 2  # وقفه بین هر دسته درخواست
+        num_requests = 20
+        num_threads = min(num_requests, 20)
+        delay_between_batches = 2  
         
         successful_requests = 0
         errors = []
         datas = []
 
-        starttime = time.time()  # زمان شروع
+        starttime = time.time()  
 
-        # استفاده از ThreadPoolExecutor برای مالتی‌تردینگ
         with ThreadPoolExecutor(max_workers=num_threads) as executor:
             for i in range(0, num_requests, num_threads):
                 futures = [executor.submit(fetch_data, url) for _ in range(num_threads)]
                 
-                # جمع‌آوری نتایج
+                
                 for future in futures:
                     result = future.result()
-                    if result:
+                    if result and 'error' not in result:
                         successful_requests += 1
                         datas.append(result)
                     else:
                         errors.append(result.get("error", "Unknown error"))
                     
 
-                # اضافه کردن وقفه بین هر دسته از درخواست‌ها
+                
                 time.sleep(delay_between_batches)
 
-        endtime = time.time()  # زمان پایان
-        totaltime = endtime - starttime  # زمان کل صرف شده
+        endtime = time.time()  
+        totaltime = endtime - starttime  
 
         return Response({
             'message': f'Total requests: {num_requests}',
             'successful_requests': successful_requests,
             'errors': errors,
-            'total_time': f'{totaltime:.2f} seconds',  # زمان کل
+            'total_time': f'{totaltime:.2f} seconds',  
             'datas': datas
         }, status=status.HTTP_200_OK)
 
--- binance_project/binance_project/settings.py
+++ binance_project/binance_project/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = 'binance_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+   
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_NAME"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),  
+        'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
 
